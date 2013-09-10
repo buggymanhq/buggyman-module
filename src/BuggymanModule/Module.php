9857f1a7406af56cb04629bf8e5bf489f4fac1fc
Evgeny Shpilevsky <evgeny@shpilevsky.com>
2013-09-10 19:24:25 +0300

    change js api. to cdn.

--- src/BuggymanModule/Module.php
+++ src/BuggymanModule/Module.php
@@ -48,7 +48,7 @@ class Module implements BootstrapListenerInterface, ConfigProviderInterface, Ser
             $pluginManager = $serviceManager->get('ViewHelperManager');
             /** @var InlineScript $inline */
             $inline = $pluginManager->get('InlineScript');
-            $inline($inline::FILE, 'http://api.buggyman.io/v1/js/' . $options->getToken() . '/collector.js');
+            $inline($inline::FILE, 'http://cdn.buggyman.io/v1/js/' . $options->getToken() . '/collector.js');
         }
     }
 
